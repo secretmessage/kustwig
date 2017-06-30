64fae63a1edfb629e55aa5343c7988d724ea7042
Kushal Hada <kushaldeveloper@gmail.com>
2017-06-30 17:51:56 -0400

    use for each loop
    chapter 16 for loop

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ $name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
 if (!$name) {
     $name = "Mercury, Venus, Mars, Jupiter, Saturn, Uranus & Neptune.";
 }
+
+$planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 echo $twig->render('index.twig', array(
     'name' => $name,
+    'planets' => $planets,
 ));
