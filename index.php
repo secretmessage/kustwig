83baa402a470934310a1524255c9fcd6771e2c65
Kushal Hada <kushaldeveloper@gmail.com>
2017-07-05 10:21:43 -0400

    practice twig
    The following example removes all HTML tags from the name and title-cases it:
    1 {{ name|striptags|title }}

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ $name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
 if (!$name) {
     $name = "Mercury, Venus, Mars, Jupiter, Saturn, Uranus & Neptune.";
 }
+$htmlFields = "tHe LiFe Of ThE wIfE iS eNdEd By ThE kNiFe. this is a new sentence. this is yet another sentence.";
 
 $planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 echo $twig->render('index.twig', array(
     'name' => $name,
     'planets' => $planets,
+    'htmlFields' => $htmlFields,
 ));
